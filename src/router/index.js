dcb9a87d9bd36416e22b1e1d24a666d000b56a72
VENKAT260897 <venkatg@samisen.io>
2021-05-06 07:19:18 +0530

    Footer updated

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Drawing from '../components/Drawing.vue'
 import Photography from '../components/Photography.vue'
 import Sculpture from '../components/Sculpture.vue'
 import Login from '../components/Login.vue'
-import Testjson from '../components/Testjson.vue'
+
 import Regiesterpage from '../components/Regiesterpage.vue'
 import Artwork from '../components/Artwork.vue'
 Vue.use(VueRouter)
@@ -21,48 +21,37 @@ const routes = [{
         path: '/Artwork',
         name: 'Artwork',
         component: Artwork
-    }, {
-        path: '/Testjson',
-        name: 'Testjson',
-        component: Testjson
-
     },
+
     {
         path: '/Crafts',
         name: 'Crafts',
         component: Crafts
-    },
-    {
+    }, {
         path: '/Digital',
         name: 'Digital',
         component: Digital
-    },
-    {
+    }, {
         path: '/Drawing',
         name: 'Drawing',
         component: Drawing
-    },
-    {
+    }, {
         path: '/Photography',
         name: 'Photography',
         component: Photography
-    },
-    {
+    }, {
         path: '/Sculpture',
         name: 'Sculpture',
         component: Sculpture
-    },
-    {
+    }, {
         path: '/Login',
         name: 'Login',
         component: Login
-    },
-    {
+    }, {
         path: '/Regiesterpage',
         name: 'Regiesterpage',
         component: Regiesterpage
-    },
-    {
+    }, {
         path: '/about',
         name: 'About',
         // route level code-splitting
