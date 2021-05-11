536294b47a95107df3f71a882013f5d6b623f9e5
VENKAT260897 <venkatg@samisen.io>
2021-05-11 15:33:45 +0530

    Art work responsive

--- src/components/Artwork.vue
+++ src/components/Artwork.vue
@@ -5,111 +5,98 @@
     <navbar />
     <br />
     <Carousel3d />
+    <br />
     <b-container>
-         
-      <b-card no-body class="overflow-hidden" style="border: none; width:1200px; padding-top:20px">
-
-         <b-row no-gutters>
-            <b-col md="7">
-              
-               <b-card-body>
-              <h2>Karaoke Old Man x Amrit</h2>
-              <br />
-              <h6 class="display-6">Art informatation</h6>
-              <b-card-text>
-                <br />
-                Description
-              </b-card-text>
-              <b-card-text style="text-align: justify; padding-right:30px;">
-                Iron Man is a fictional superhero appearing in American comic
-                books published by Marvel Comics. The character was co-created
-                by writer and editor Stan Lee, developed by scripter Larry
-                Lieber, and designed by artists Don Heck and Jack Kirby. The
-                character made his first appearance in Tales of Suspense #39
-                (cover dated March 1963), and received his own title in Iron Man
-                #1 (May 1968). Also in 1963, the character founded the Avengers
-                alongside Thor, Ant-Man, Wasp and the Hulk. A wealthy American
-                business magnate, playboy, philanthropist, inventor and
-                ingenious scientist, Anthony Edward "Tony" Stark suffers a
-                severe chest injury during a kidnapping. When his captors
-                attempt to force him to build a weapon of mass destruction, he
-                instead creates a mechanized suit of armor to save his life and
-                escape captivity. Later, Stark develops his suit, adding weapons
-                and other technological devices he designed through his company,
-                Stark Industries. He uses the suit and successive versions to
-                protect the world as Iron Man. Although at first concealing his
-                true identity, Stark eventually publicly reveals himself to be
-                Iron Man. Initially, Iron Man was a vehicle for Stan Lee to
-                explore Cold War themes, particularly the role of American
-                technology and industry in the fight against communism.
-                Subsequent re-imaginings of Iron Man have transitioned from Cold
-                War motifs to contemporary matters of the time.[1]<br />
-              </b-card-text>
-              <br />
-            </b-card-body>           
-            </b-col>
-            <b-col md="5">
-              <b-card-body>
-                <br />
-                <b-row>
-                  <b-col col md="3">
-                    <b-avatar
-                      size="5rem"
-                      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-chris-hemsworth-attends-the-tag-heuer-museum-in-motion-news-photo-968210608-1554829603.jpg"
-                    >
-                    </b-avatar>
-                  </b-col>
-                  <b-col col md="8">
-                    <br />
-                    <a
-                      href="https://marvelcinematicuniverse.fandom.com/wiki/Iron_Man"
-                    >
-                      <h3>@venkat</h3></a
-                    >
-                  </b-col>
-                </b-row>                
-
-                <hr />
-                <b-card-text>
-                  <br />
-                  <h1><p class="font-weight-bold">venkat</p></h1>
-                </b-card-text>
-                <b-card-text>
-                  <br />
-                  <h4>PhotoPrice</h4>
-                  <h3>5000 Rs</h3>
-                </b-card-text>
-                <b-card-text>
-                  <br />
-                  
-                    <b-row>
-                      <b-col col md="8">
-                        <b-card-text>
-                          <b-button block href="/Artwork" variant="outline-dark">Add to Cart</b-button
-                          >
-                           <b-button block href="/Artwork" variant="outline-dark">Add to Favourite</b-button
-                          >
-                        </b-card-text>
-                      </b-col>
-                   
-                    </b-row>
-                   
-                </b-card-text>
-              </b-card-body>
+      <h4 class="display-4">TonyStrack</h4>
+      <br />
+      <h6 class="display-6">Art informatation</h6>
+      <b-row no-gutters>
+        <b-col md="8">
+          <b-card-text>
+            <br />
+            Description
+          </b-card-text>
+          <b-row>
+            <b-col col sm="10">
+          <b-card-text>
+            Iron Man is a fictional superhero appearing in American comic books
+            published by Marvel Comics. The character was co-created by writer
+            and editor Stan Lee, developed by scripter Larry Lieber, and
+            designed by artists Don Heck and Jack Kirby. The character made his
+            first appearance in Tales of Suspense #39 (cover dated March 1963),
+            and received his own title in Iron Man #1 (May 1968). Also in 1963,
+            the character founded the Avengers alongside Thor, Ant-Man, Wasp and
+            the Hulk. A wealthy American business magnate, playboy,
+            philanthropist, inventor and ingenious scientist, Anthony Edward
+            "Tony" Stark suffers a severe chest injury during a kidnapping. When
+            his captors attempt to force him to build a weapon of mass
+            destruction, he instead creates a mechanized suit vehicle for Stan
+            Lee to explore Cold War themes, particularly the role of American
+            technology and industry in the fight against communism. Subsequent
+            re-imaginings of Iron Man have transitioned from Cold War motifs to
+            contemporary matters of the time.[1]<br />
+          </b-card-text>
             </b-col>
           </b-row>
-      </b-card>
-    </b-container>
+          <br />
+        </b-col>
 
-<br>
-  
+        <b-col md="4">
+          <b-card-text>
+            <b-row>
+              <b-col col md="3">
+                <b-avatar
+                  size="4rem"
+                  src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-chris-hemsworth-attends-the-tag-heuer-museum-in-motion-news-photo-968210608-1554829603.jpg"
+                >
+                </b-avatar>
+              </b-col>
+              <b-col col md="8">
+                <br />
+                <a
+                  href="https://marvelcinematicuniverse.fandom.com/wiki/Iron_Man"
+                >
+                  <h3>@venkat</h3></a
+                >
+              </b-col>
+            </b-row>
+            <b-card-text>
+              <br />
+              <h1><p class="font-weight-bold">venkat</p></h1>
+            </b-card-text>
+            <b-card-text>
+              <br />
+              <h4>PhotoPrice</h4>
+              <h3>5000 Rs</h3>
+            </b-card-text>
+            <b-card-text>
+              <br />
+
+              <b-row>
+                <b-col col md="8">
+                  <b-card-text>
+                    <b-button block href="/Artwork" variant="outline-dark"
+                      >Add to Cart</b-button
+                    >
+                    <b-button block href="/Artwork" variant="outline-dark"
+                      >Add to Favourite</b-button
+                    >
+                  </b-card-text>
+                </b-col>
+              </b-row>
+            </b-card-text>
+            <br />
+          </b-card-text>
+        </b-col>
+      </b-row>
+    </b-container>
+    <br />
     <b-list-group>
+      <h5 style="padding-left: 50px"><b>Creator</b></h5>
       <b-list-group-item>
         <b-container>
-           <h5 style="padding-left:50px;"><b>Creator</b></h5>
-              <br>
+          <br />
           <b-row>
-             
             <b-col col md="2">
               <b-avatar
                 button
@@ -140,6 +127,7 @@
       </b-list-group-item>
       <br />
     </b-list-group>
+
     <Footer />
   </div>
 </template>
@@ -160,4 +148,5 @@ export default {
 
 
 <style>
+
 </style>
--- src/components/Carousel3d.vue
+++ src/components/Carousel3d.vue
@@ -1,69 +1,87 @@
 <template>
   <div id="app">
-      <b-jumbotron   bg-variant="grey" text-variant="white" border-variant="grey"  >
-    <carousel-3d
-      :controls-visible="true"
-      :clickable="false"
-      :autoplay="true"
-      :autoplayTimeout="3500"
-      :width="410"
-      :height="350"
-    >
-      <slide v-for="(slide, i) in slides" :key="i" :index="i">
-        <figure>
+    <b-jumbotron bg-variant="grey" text-variant="white" border-variant="grey ">
+      <carousel-3d
+        :controls-visible="true"
+        :clickable="false"
+        :autoplay="true"
+        :autoplayTimeout="3500"
+        :width="500"
+        :height="450"
+      >
+        <slide :index="0">
           <img
-            src="https://assets.hongkiat.com/uploads/really-nice-ironman-wallpapers/original/ironman-wp.jpg"
-          height="350" width="450" />
-        </figure>
-      </slide>
-    </carousel-3d>
- 
-<!-- ADD SHARE BUTTONS-->
- 
-    <div id="socialbtn">
-    <a
-      href="https://www.google.com"
-      target="_blank"
-      ><img
-        src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png"
-        border="0"
-        alt="Email"
-    /></a>
-    <a
-      href="https://api.addthis.com/oexchange/0.8/forward/facebook/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
-      target="_blank"
-      ><img
-        src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/facebook.png"
-        border="0"
-        alt="Facebook"
-    /></a>
-    <a
-      href="https://api.addthis.com/oexchange/0.8/forward/twitter/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
-      target="_blank"
-      ><img
-        src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/twitter.png"
-        border="0"
-        alt="Twitter"
-    /></a>
-    <a
-      href="https://api.addthis.com/oexchange/0.8/forward/whatsapp/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
-      target="_blank"
-      ><img
-        src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/whatsapp.png"
-        border="0"
-        alt="WhatsApp"
-    /></a>
-    <a
-      href="https://api.addthis.com/oexchange/0.8/forward/gmail/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
-      target="_blank"
-      ><img
-        src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/gmail.png"
-        border="0"
-        alt="Gmail"
-    /></a>
-  </div>
-       </b-jumbotron>
-
+            src="https://cdn.pixabay.com/index/2021/04/30/09-58-39-26_1440x480.jpg"
+            :width="500"
+            :height="450"
+          />
+        </slide>
+        <slide :index="1">
+          <img
+            src="https://www.10wallpaper.com/wallpaper/1366x768/1107/Window_pastoral-the_worlds_high-definition_nature_photography_1366x768.jpg"
+            :width="500"
+            :height="450"
+          />
+        </slide>
+        <slide :index="2">
+          <img
+            src="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270"
+            :width="500"
+            :height="450"
+          />
+        </slide>
+        <slide :index="3">
+          <img
+            src="https://i.pinimg.com/originals/1c/aa/c5/1caac55143e3e11461c6ae5962403deb.jpg"
+            :width="500"
+            :height="450"
+          />
+        </slide>
+        <slide :index="4">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/11/10/22/57/nature-3807667_960_720.jpg"
+            :width="500"
+            :height="450"
+          />
+        </slide>
+      </carousel-3d>
+      <!-- ADD SHARE BUTTONS-->
+      <br />
+      <div id="socialbtn">
+        <a
+          href="https://api.addthis.com/oexchange/0.8/forward/facebook/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
+          target="_blank"
+          ><img
+            src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/facebook.png"
+            border="0"
+            alt="Facebook"
+        /></a>
+        <a
+          href="https://api.addthis.com/oexchange/0.8/forward/twitter/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
+          target="_blank"
+          ><img
+            src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/twitter.png"
+            border="0"
+            alt="Twitter"
+        /></a>
+        <a
+          href="https://api.addthis.com/oexchange/0.8/forward/whatsapp/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
+          target="_blank"
+          ><img
+            src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/whatsapp.png"
+            border="0"
+            alt="WhatsApp"
+        /></a>
+        <a
+          href="https://api.addthis.com/oexchange/0.8/forward/gmail/offer?url=https%3A%2F%2Fwww.addthis.com%2F&pubid=ra-42fed1e187bae420&title=AddThis%20%7C%20Home&ct=1"
+          target="_blank"
+          ><img
+            src="https://cache.addthiscdn.com/icons/v3/thumbs/32x32/gmail.png"
+            border="0"
+            alt="Gmail"
+        /></a>
+      </div>
+    </b-jumbotron>
   </div>
 </template>
 
@@ -89,21 +107,19 @@ export default {
   margin: 0;
 }
 
-
- #socialbtn {  
+#socialbtn {
   position: relative;
-  top:-65px; 
+  top: -65px;
   float: right;
-   background-color: lightgrey;
-   border : none;
-}  
-
+  background-color: lightgrey;
+  border: none;
+}
 
 * {
   box-sizing: border-box;
 }
- 
- 
-.jumbotron { height: 400px; }
 
+.jumbotron {
+  height: 535px;
+}
 </style>
