be5b40b9005f9c544191dce4fc253b473df3292a
VENKAT260897 <venkatg@samisen.io>
2021-05-21 12:23:33 +0530

    Update artwork.js

--- src/mixins/artwork.js
+++ src/mixins/artwork.js
@@ -1,8 +1,8 @@
 export default {
     data() {
         return {
-            pictureIdFromUrl: "",
-            artworkTypeFromUrl: "",
+            getpictureID: "",
+            getartworktype: "",
         };
     },
 
@@ -10,8 +10,8 @@ export default {
         url: function() {
             var str = window.location.search;
             var val = str.split("?").pop();
-            this.pictureIdFromUrl = str.split("=").pop();
-            this.artworkTypeFromUrl = val.substring(0, val.indexOf("="));
+            this.getpictureID = str.split("=").pop();
+            this.getartworktype = val.substring(0, val.indexOf("="));
         },
     },
     mounted() {
@@ -20,45 +20,45 @@ export default {
     computed: {
         artwork() {
             var jsonResult;
-            if (this.artworkTypeFromUrl == "featureArtworkId") {
+            if (this.getartworktype == "featureArtworkId") {
                 this.$store.dispatch("fetchFeatureds");
                 jsonResult = this.$store.state.featureds.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
-            } else if (this.artworkTypeFromUrl == "highestPaidId") {
+            } else if (this.getartworktype == "highestPaidId") {
                 this.$store.dispatch("fetchHighestPaids");
                 jsonResult = this.$store.state.highestpaids.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
-            } else if (this.artworkTypeFromUrl == "topArtistsId") {
+            } else if (this.getartworktype == "topArtistsId") {
                 this.$store.dispatch("fetchTopArtists");
                 jsonResult = this.$store.state.topartists.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
-            } else if (this.artworkTypeFromUrl == "digitalArtworkId") {
+            } else if (this.getartworktype == "digitalArtworkId") {
                 this.$store.dispatch("fetchDigitals");
                 jsonResult = this.$store.state.digitals.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
-            } else if (this.artworkTypeFromUrl == "craftArtworkId") {
+            } else if (this.getartworktype == "craftArtworkId") {
                 this.$store.dispatch("fetchCrafts");
                 jsonResult = this.$store.state.crafts.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
-            } else if (this.artworkTypeFromUrl == "photographyArtworkId") {
+            } else if (this.getartworktype == "photographyArtworkId") {
                 this.$store.dispatch("fetchPhotographys");
                 jsonResult = this.$store.state.photographys.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
-            } else if (this.artworkTypeFromUrl == "sculptureArtworkId") {
+            } else if (this.getartworktype == "sculptureArtworkId") {
                 this.$store.dispatch("fetchSculptures");
                 jsonResult = this.$store.state.sculptures.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
-            } else if (this.artworkTypeFromUrl == "drawingArtworkId") {
+            } else if (this.getartworktype == "drawingArtworkId") {
                 this.$store.dispatch("fetchDrawings");
                 jsonResult = this.$store.state.drawings.find(
-                    (obj) => obj.Id == this.pictureIdFromUrl
+                    (obj) => obj.Id == this.getpictureID
                 );
             }
             return jsonResult;
